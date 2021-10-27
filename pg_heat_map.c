4b6bc798567bb4500e7e3bd07c0bd68c4ff0b1a3
itatsiy <itatsiy@dpd.ru>
2021-10-28 01:50:13 +0300

    distribution and integer[] function result

--- pg_heat_map.c
+++ pg_heat_map.c
@@ -1,23 +1,40 @@
 #include "postgres.h"
+
+#include <limits.h>
+
 #include "fmgr.h"
 #include "utils/builtins.h"
 #include "utils/geo_decls.h"
 #include "utils/array.h"
 #include "utils/arrayaccess.h"
+#include "utils/float.h"
+#include "utils/lsyscache.h"
 
 PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(build_heat_map);
 
 typedef struct {
-    int current;
-} build_heat_map_func_struct;
+    float8 xMin;
+    float8 yMin;
+    int xBucketSize;
+    int yBucketSize;
+} PrebuildDesc;
 
-Point* defineMapLimits(Point *array, int size, Point *leftTop, Point *rightBottom) {
-    Point lt = *leftTop;
-    Point rb = *rightBottom;
+typedef struct {
+    int * distributed;
+    int len;
+    int min;
+    int max;
+} DistributionDesc;
+
+PrebuildDesc
+defineMapLimits(Point *array, int size, float8 resolution) {
+    Point first = *DatumGetPointP(&array[0]);
+    Point lt = (Point) {.x = first.x, .y = first.y};
+    Point rb = (Point) {.x = first.x, .y = first.y};
     int i;
-    for (i = 0; i < size; i++) {
+    for (i = 1; i < size; i++) {
         Point value = *DatumGetPointP(&array[i]);
         if (lt.x > value.x) {
             lt.x = value.x;
@@ -30,46 +47,84 @@ Point* defineMapLimits(Point *array, int size, Point *leftTop, Point *rightBotto
             rb.y = value.y;
         }
     }
-    leftTop -> x = lt.x;
-    leftTop -> y = lt.y;
-    rightBottom -> x = rb.x;
-    rightBottom -> y = rb.y;
-    Point * result = (Point *) palloc(sizeof(Point));
-    result -> x = rb.x - lt.x;
-    result -> y = lt.y - rb.y;
-    return result;
+    return (PrebuildDesc) {
+            .xMin = lt.x,
+            .yMin = rb.y,
+            .xBucketSize = (int) ((rb.x - lt.x) / resolution),
+            .yBucketSize = (int) ((lt.y - rb.y) / resolution),
+    };
 }
 
-Point * createPoint(Point *source) {
-    Point * result = (Point *) palloc(sizeof(Point));
-    result -> x = source -> x;
-    result -> y = source -> y;
+DistributionDesc
+distribute(Point *array, int size, float resolution, PrebuildDesc prebuildDesc) {
+    int resultArraySize = prebuildDesc.xBucketSize * prebuildDesc.yBucketSize;
+    int *result = (int *) palloc(resultArraySize * sizeof(int));
+    memset(result, 0, resultArraySize * sizeof(int));
+    int max = 0;
+    int min = INT_MAX;
+    int i;
+    for (i = 0; i < size; i++) {
+        Point value = *DatumGetPointP(&array[i]);
+        int x = (value.x - prebuildDesc.xMin) / resolution;
+        int y = (value.y - prebuildDesc.yMin) / resolution;
+        int current = ++result[y * prebuildDesc.yBucketSize + x];
+        if (max < current) {
+            max = current;
+        }
+        if (min > current) {
+            min = current;
+        }
+    }
+    return (DistributionDesc) {
+        .distributed = result,
+        .len = resultArraySize,
+        .min = min,
+        .max = max
+    };
+}
+
+Datum *
+normalize(DistributionDesc distributionDesc) {
+    Datum *result = (Datum *) palloc(distributionDesc.len * sizeof(Datum));
+    int i;
+    for (i = 0; i < distributionDesc.len; i++) {
+        int value = distributionDesc.distributed[i];
+        result[i] = Int32GetDatum(value);
+    }
     return result;
 }
 
 Datum
 build_heat_map(PG_FUNCTION_ARGS) {
+    ArrayType *result;
+    Datum *resultDatum;
     // Define input
     AnyArrayType *arg1AsArrayType = PG_GETARG_ANY_ARRAY_P(0);
+    float8 resolution = PG_GETARG_FLOAT8(1);
+
     // Define variables
     Point *array;
     int arraySize;
-    Point *leftTop;
-    Point *rightBottom;
-    Point *first;
-    Point *diff;
-    // Dimension has to be equal 1
-    if (AARR_NDIM(arg1AsArrayType) != 1)
+    PrebuildDesc prebuildDesc;
+    DistributionDesc distributionDesc;
+
+    // Validation
+    if (isinf(resolution) || AARR_NDIM(arg1AsArrayType) != 1)
         PG_RETURN_NULL();
-    // Assign array params after the check
     arraySize = AARR_DIMS(arg1AsArrayType)[0];
     if (arraySize == 0)
         PG_RETURN_NULL();
-    array = (Point *) ARR_DATA_PTR(&(arg1AsArrayType->flt));
-    first = &array[0];
-    leftTop = createPoint(first);
-    rightBottom = createPoint(first);
-    diff = defineMapLimits(array, arraySize, leftTop, rightBottom);
 
-    PG_RETURN_POINT_P(diff);
+    array = (Point *) ARR_DATA_PTR(&(arg1AsArrayType->flt));
+
+    prebuildDesc = defineMapLimits(array, arraySize, resolution);
+
+    distributionDesc = distribute(array, arraySize, resolution, prebuildDesc);
+    resultDatum = normalize(distributionDesc);
+    int16 typlen;
+    bool typbyval;
+    char typalign;
+    get_typlenbyvalalign(INT4OID, &typlen, &typbyval, &typalign);
+    result = construct_array(resultDatum, distributionDesc.len, INT4OID, typlen, typbyval, typalign);
+    PG_RETURN_ARRAYTYPE_P(result);
 }
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
+psql -c 'drop extension if exists pg_heat_map'
 make
 make install
 make installcheck
-psql -c 'drop extension if exists pg_heat_map'
 psql -c 'create extension pg_heat_map'
-psql -c 'select build_heat_map(ARRAY [point(1, 2), point(2, 3)])'
+psql -c 'select build_heat_map(ARRAY [point(1, 2), point(2, 3)], 0.1)'
